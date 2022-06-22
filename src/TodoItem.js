814d4985d23902ad6ad78723475db511d97e94a5
youwei <1690312648@qq.com>
2022-06-23 00:08:47 +0800

    4.2 propTypes和defaultProps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 export default class TodoItem extends Component {
   constructor(props) {
     super(props);
@@ -6,8 +8,12 @@ export default class TodoItem extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
   render() {
-    const { item } = this.props;
-    return <div onClick={this.handleClick}>{item}</div>;
+    const { item, test } = this.props;
+    return (
+      <div onClick={this.handleClick}>
+        {test} -- {item}
+      </div>
+    );
   }
   handleClick() {
     // 调用父组件传递的方法
@@ -15,3 +21,18 @@ export default class TodoItem extends Component {
     deleteItem(index);
   }
 }
+
+// 对TodoItem组件的props做校验
+TodoItem.propTypes = {
+  // isRequired 必传,不加就是可传可不传，不传就不进行类型校验
+  // oneOfType 或者
+  test: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  item: PropTypes.string.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+// prop默认值
+TodoItem.defaultProps = {
+  test: 12,
+};
