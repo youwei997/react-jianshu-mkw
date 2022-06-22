ae460eafb81be57c8ead4f332969a8b803a4a1b7
youwei <1690312648@qq.com>
2022-06-22 18:13:35 +0800

    3-4实现react todolist新增删除

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,8 +9,8 @@ class TodoList extends React.Component {
     // 组件的状态
     // 必须得写这一句
     this.state = {
-      inputValue: "111",
-      list: [],
+      inputValue: "",
+      list: ["react", "vue"],
     };
   }
 
@@ -23,6 +23,25 @@ class TodoList extends React.Component {
     // console.log(e.target.value);
   }
 
+  // button 提交事件
+  handleSubmit() {
+    this.setState({
+      // list 先合并旧的list，在合并新的inputValue
+      list: [...this.state.list, this.state.inputValue],
+      //在清空inputValue
+      inputValue: "",
+    });
+  }
+  // 删除事件
+  handleDelete(index) {
+    // immutable
+    // react 不允许直接修改state
+    // 必须得把list拷贝一份出来修改，然后在setState
+    const list = this.state.list;
+    list.splice(index, 1);
+    this.setState({ list });
+  }
+
   render() {
     return (
       // Fragment占位标签，不渲染到dom上，类似vue中的template
@@ -33,11 +52,16 @@ class TodoList extends React.Component {
             value={this.state.inputValue}
             onChange={this.handleInputChange.bind(this)}
           />
-          <button>提交</button>
+          <button onClick={this.handleSubmit.bind(this)}>提交</button>
         </div>
         <ul>
-          <li>li1</li>
-          <li>li2</li>
+          {this.state.list.map((item, index) => {
+            return (
+              <li key={index} onClick={this.handleDelete.bind(this, index)}>
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </Fragment>
     );
