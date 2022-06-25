dd1826a7f8fd8d84bb76aba52294abeb44fdbf04
youwe1 <1690312648@qq.com>
2022-06-25 15:21:28 +0800

    5-9 redux知识点复习补充

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,17 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
 
+// store---------
+// 必须是唯一的，一个应用只有一个store公共储存
+// 只有store才能改变自己的内容
+// reducer必须是纯函数（给固定的输入，有固定的输出）
+
+// redux api
+// createStore 创建一个store
+// store.dispatch 派发一个store
+// store.getState 获取到store的所有数据内容
+// store.subscribe 订阅store的改变
+
 // createStore 传入 reducer函数
 const store = createStore(
   reducer,
