35575b9ef60fd99d530a580e184763c25a37e15f
youwei <1690312648@qq.com>
2022-06-29 15:54:45 +0800

    7-9使用Immutable.js来管理store中的数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,20 @@
 // header reducer 专门对header的数据和操作
 import { SEARCH_FOCUS, SEARCH_BLUR } from "./actionTypes";
-const defaultState = {
+import { fromJS } from "immutable";
+// immutable 库
+// immutable 对象
+
+// fromJS 把普通的js对象改成immutable对象
+const defaultState = fromJS({
   focused: false,
-};
+});
 export default (state = defaultState, action) => {
   if (action.type === SEARCH_FOCUS) {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.focused = true;
-    return newState;
+    // immutable 对象的set方法，会结合之期的immutable对象的值和设置的值，返回一个全新的值
+    return state.set("focused", true);
   }
   if (action.type === SEARCH_BLUR) {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.focused = false;
-    return newState;
+    return state.set("focused", false);
   }
 
   return state;
